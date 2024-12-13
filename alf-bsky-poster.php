13490af0f5d5ef221f2957e59797afdc1b422628
Mark Biek <mark.biek@automattic.com>
2024-12-13 13:37:51 -0500

    Basic dev environment is set up

---
+++ alf-bsky-poster.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: ALF Bluesky Poster
+ * Plugin URI: https://github.com/mark.biek.org/alf-bsky-poster
+ * Description: A WordPress plugin to automatically post content to Bluesky
+ * Version: 0.1.0
+ * Author: Mark Biek
+ * Author URI: https://mark.biek.org
+ * Text Domain: alf-bsky-poster
+ * Domain Path: /languages
+ * Requires PHP: 8.1
+ * Requires at least: 6.0
+ *
+ * @category  Social_Media
+ * @package   ALF_Bsky_Poster
+ * @author    Mark Biek <mark@biek.org>
+ * @copyright 2024 Mark Biek
+ * @license   GPL v2 or newer <https://www.gnu.org/licenses/gpl.txt>
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
