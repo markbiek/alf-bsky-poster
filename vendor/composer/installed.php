18de7e60975f71e8180fd1806f36849e55718443
Mark Biek <mark.biek@automattic.com>
2024-12-16 13:27:32 -0500

    Include vendor directory
    This only includes the composer stuff necessary for our namespace. There are no external libraries

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'markbiek/alf-bsky-poster',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0d7e3ec8e791ef288492ed219d44806f10d01449',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'markbiek/alf-bsky-poster' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0d7e3ec8e791ef288492ed219d44806f10d01449',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
